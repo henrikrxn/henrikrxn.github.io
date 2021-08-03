e6b8878f5257450dc0965c8a8ab3be5767d76f59
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2021-08-03 16:12:31 +0200

    Using Devlead.Statiq to add theme

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Devlead.Statiq.Themes;
 using Statiq.App;
 using Statiq.Web;
 
@@ -9,7 +11,9 @@ namespace StatiqWebQuickStartWithCleanBlogTheme
         public static async Task<int> Main(string[] args) =>
           await Bootstrapper
             .Factory
-            .CreateWeb(args)
+            .CreateDefault(args)
+            .AddThemeFromUri(new Uri("https://github.com/statiqdev/CleanBlog/archive/ceb5055f3d0f7a330708494ed21eb469cde62ce2.zip"))
+            .AddWeb()
             .RunAsync();
     }
 }
