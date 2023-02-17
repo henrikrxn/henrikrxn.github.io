7a4e4b5a78c9d13df546d948a4546e5d5b7847ec
henrikrxn <20686751+henrikrxn@users.noreply.github.com>
2023-02-17 18:50:45 +0100

    Removing Devlead.Statiq because no longer needed

--- src/henrikrxn.github.io/Program.cs
+++ src/henrikrxn.github.io/Program.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using Statiq.App;
 using Statiq.Common;
@@ -12,7 +11,6 @@ namespace Website
           await Bootstrapper
             .Factory
             .CreateDefault(args)
-            .AddThemeFromUri(new Uri("https://github.com/henrikrxn/CleanBlog/archive/de74dd28971e0634dd28b6437e327462991cc824.zip"))
             .DeployToGitHubPagesBranch(
                 owner: "henrikrxn",
                 name: "henrikrxn.github.io",
